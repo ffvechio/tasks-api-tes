559e5cb3ed7d99e28b03f3d0896a1e4ec5d45f8e
Felipe Bessa <ffvechio@gmail.com>
2020-04-02 21:28:37 -0300

    first commit

---
+++ src/test/java/br/com/felipebessa/tasks/apitest/APITest.java
@@ -0,0 +1,50 @@
+package br.com.felipebessa.tasks.apitest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class APITest {
+
+    @BeforeClass
+    public static void setup() {
+        RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+    }
+
+    @Test
+    public void deveRetornarTarefas() {
+        RestAssured
+                .given()
+                .when()
+                .get("/todo")
+                .then().statusCode(200)
+                .log().all();
+    }
+
+    @Test
+    public void deveAdicionarTarefaComSucesso() {
+        RestAssured
+                .given().contentType(ContentType.JSON)
+                .body("{\"task\": \"Teste automation\", \"dueDate\": \"2020-12-30\"}")
+                .when()
+                .post("/todo")
+                .then().statusCode(201)
+                .log().all();
+    }
+
+    @Test
+    public void naoDeveAdicionarTarefaInvalida() {
+        RestAssured
+                .given().contentType(ContentType.JSON)
+                .body("{\"task\": \"Teste automation\", \"dueDate\": \"2010-12-30\"}")
+                .when()
+                .post("/todo")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .body("message", CoreMatchers.is("Due date must not be in past"));
+    }
+}
+
